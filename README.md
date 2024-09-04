21a8b1eb2ae0d46963dd4d12c82bc7245bd752a7
Joudy NADER <joudy_nader@hotmail.com>
2024-09-04 19:21:42 +0200

    Modify gpsd.py and modify README.md

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ sudo apt install libgdal-dev
 pip3 install -r requirements.txt
 ```
 
+# Known issues
+
+- When importing numpy, you may get the following error:
+```
+python3 -c "import numpy"
+Illegal instruction (core dumped)
+```
+
+To counter this error, do:
+
+```
+OPENBLAS_CORETYPE=ARMV8 python3 -c "import numpy"
+```
+
--- gpsd.py
+++ gpsd.py
@@ -1,6 +1,7 @@
 import time
 
 from gps import Sim7600Module
+from coordinates import Coordinates
 
 if __name__ == "__main__":
     board = Sim7600Module()
@@ -9,9 +10,15 @@ if __name__ == "__main__":
     try:
         if board.is_open:
             while 1:
+                data: Coordinates
                 data = board.get_gps_position()
-                if data[0] != "":
-                    print(data)
+                if data is not None:
+                    print(
+                        data.latitude,
+                        data.latitude_indicator,
+                        data.longitude,
+                        data.longitude_indicator,
+                    )
                 time.sleep(3)
 
         board.close()
