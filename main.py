2406396782980fe15eb15b6e841fbe9f07c6188f
Joudy NADER <joudy_nader@hotmail.com>
2024-09-22 16:14:37 +0200

    Check for None values in main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from gps import Sim7600Module
 from coordinates import Coordinates
 from zone import Zone
 
+
 def in_out_zone(flag):
     print(f"In/Out Zone {flag}")
 
@@ -29,13 +30,14 @@ if __name__ == "__main__":
         # Load GPS module
         board.open()
         if board.is_open:
-            gps_module_position_epsg_4326: Coordinates
-            gps_module_position_epsg_4326 = board.get_gps_position()
-            gps_module_position_epsg_zone = gps_module_position_epsg_4326.to_crs(
-                zone.zone_expanded.crs.to_epsg()
-            )
+            gps_module_position_epsg_4326: Coordinates = None
+            while gps_module_position_epsg_4326 is None:
+                gps_module_position_epsg_4326 = board.get_gps_position()
+                gps_module_position_epsg_zone = gps_module_position_epsg_4326.to_crs(
+                    zone.zone_expanded.crs.to_epsg()
+                )
 
-            in_out_zone(zone.intersects(gps_module_position_epsg_zone))
+                in_out_zone(zone.intersects(gps_module_position_epsg_zone))
 
             board.close()
 
